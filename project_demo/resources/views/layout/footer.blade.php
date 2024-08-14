df3793d2353df701aebbc3f723ff8d7b12623c14
Raihan-Raju <idbraihan58@gmail.com>
2024-08-14 19:05:57 +0600

    laravel

---
+++ project_demo/resources/views/layout/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+
+<div class="footer-bottom">
+
+    <div class="container text-right">
+        Sadaka @ copyrights 2015 - by <a href="http://www.ouarmedia.com" target="_blank">Ouarmedia</a>
+    </div>
+</div>
+
+</footer> 
---
+++ project_demo/resources/views/master.blade.php
@@ -0,0 +1,304 @@
+
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>SADAKA | Charity / Non-profit responsive Bootstrap HTML5 template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Dosis:400,700' rel='stylesheet' type='text/css'>
+
+        <!-- Bootsrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <!-- Owl carousel -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+
+        <!-- Template main Css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <!-- Modernizr -->
+        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+
+
+    </head>
+
+    <body>
+
+
+    <header class="main-header">
+        
+    <!-- nav -->
+   @include('layout.navber')
+       
+    </header> <!-- /. main-header -->
+
+
+
+
+    <!-- Carousel
+   
+    ================================================== -->
+    @include('layout.carajol')
+
+
+
+    <!-- <div class="section-home home-reasons">
+
+        <div class="container">
+
+            <div class="row">
+                
+                <div class="col-md-6">
+
+                    <div class="reasons-col animate-onscroll fadeIn">
+
+                        <img src="assets/images/reasons/we-fight-togother.jpg" alt="">
+
+                        <div class="reasons-titles">
+
+                            <h3 class="reasons-title">We fight together</h3>
+                            <h5 class="reason-subtitle">We are humans</h5>
+                            
+                        </div>
+
+                        <div class="on-hover hidden-xs">
+                            
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>
+
+
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>
+
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>
+                                
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+                <div class="col-md-6">
+
+                    <div class="reasons-col animate-onscroll fadeIn">
+
+                        <img src="assets/images/reasons/we-care-about.jpg" alt="">
+
+                        <div class="reasons-titles">
+
+                            <h3 class="reasons-title">WE care about others</h3>
+                            <h5 class="reason-subtitle">We are humans</h5>
+                            
+                        </div>
+
+                        <div class="on-hover hidden-xs">
+                            
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>
+
+
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>
+
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>
+                                
+                        </div>
+
+
+                    </div>
+                    
+                </div>
+
+
+            </div>
+          
+  
+
+        </div>
+      
+
+    </div>  -->
+<!-- 
+     <div class="section-home our-causes animate-onscroll fadeIn"> -->
+
+        <!-- <div class="container">
+
+            <h2 class="title-style-1">Our Causes <span class="title-under"></span></h2>
+
+            <div class="row">
+
+                <div class="col-md-3 col-sm-6">
+
+                    <div class="cause">
+
+                        <img src="assets/images/causes/cause-hunger.jpg" alt="" class="cause-img">
+
+                        <div class="progress cause-progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">
+                            10$ / 500$
+                          </div>
+                        </div>
+
+                        <h4 class="cause-title"><a href="#">HUNGER AND POVERTY </a></h4>
+                        <div class="cause-details">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.
+                        </div>
+
+                        <div class="btn-holder text-center">
+
+                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>
+                          
+                        </div>
+
+                        
+
+                    </div> <!-- /.cause -->
+                    
+                <!-- </div> 
+
+                <div class="col-md-3 col-sm-6">
+
+                    <div class="cause">
+
+                        <img src="assets/images/causes/cause-education.jpg" alt="" class="cause-img">
+
+                        <div class="progress cause-progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                            400$ / 700$
+                          </div>
+                        </div>
+
+                        <h4 class="cause-title"><a href="#">EDUCATION AND TRAINING</a></h4>
+                        <div class="cause-details">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.
+                        </div>
+
+                        <div class="btn-holder text-center">
+
+                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>
+                          
+                        </div>
+
+                        
+
+                    </div> <!-- /.cause -->
+                    
+                <!-- </div>
+
+
+                <div class="col-md-3 col-sm-6">
+
+                    <div class="cause">
+
+                        <img src="assets/images/causes/cause-rights.jpg" alt="" class="cause-img">
+
+                        <div class="progress cause-progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+                            400$ / 1000$
+                          </div>
+                        </div>
+
+                        <h4 class="cause-title"><a href="#">HUMAN RIGHTS</a></h4>
+                        <div class="cause-details">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.
+                        </div>
+
+                        <div class="btn-holder text-center">
+
+                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>
+                          
+                        </div>
+
+                        
+
+                    </div> 
+                    
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+
+                    <div class="cause">
+
+                        <img src="assets/images/causes/cause-culture.jpg" alt="" class="cause-img">
+
+                        <div class="progress cause-progress">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                            400$ / 700$
+                          </div>
+                        </div>
+
+                        <h4 class="cause-title"><a href="#">ARTS AND CULTURE </a></h4>
+                        <div class="cause-details">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.
+                        </div>
+
+                        <div class="btn-holder text-center">
+
+                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>
+                          
+                        </div>
+
+                        
+
+                    </div> 
+                    
+                </div>
+
+            </div>
+
+        </div>  -->
+        
+    <!-- </div>   -->
+
+    
+
+
+
+
+    @include('layout.caralogo')
+   
+
+
+    
+
+
+   
+
+
+
+
+
+    @include('layout.footer')
+
+
+
+    <!--  Scripts
+    ================================================== -->
+
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.1.min.js"><\/script>')</script>
+
+    <!-- Bootsrap javascript file -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+    <!-- owl carouseljavascript file -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <!-- Template main javascript -->
+    <script src="assets/js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+
+    </body>
+</html>
